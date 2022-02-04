21c1262b6de73f1eb3855e384be1a316604bc16c
ad656 <allandong656@gmail.com>
2022-02-04 13:48:54 -0800

    added and edited demo files

--- demo.java
+++ demo.java
@@ -1,3 +1,8 @@
 public class demo{
+public static int multiply(int x, int y){
+return x*y;
 
 }
+
+
+} 
---
+++ demoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class demoTest {
+    @Test
+public void testmultiply(){
+    assertEquals(4,demo.multiply(3,3));
+}
+
+}
